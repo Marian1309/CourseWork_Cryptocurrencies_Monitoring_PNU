823584cb70a5f6e908e5d2907c5effc9f874ec71
Marian <pidchashymaryan@gmail.com>
2024-12-24 11:17:21 +0200

    fix: runtime for cloudflare

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next';
 import { redirect } from 'next/navigation';
 
+export const runtime = 'experimental-edge';
+
 const NotFound: NextPage = () => redirect('/');
 
 export default NotFound;
